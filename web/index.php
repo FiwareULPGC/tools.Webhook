0b6bd050f2d46d2ba70e1b9365ed71874cb935cd
Fiware ULPGC <fiware@ulpgc.es>
2016-05-18 16:00:04 +0100

    Add basic error support

--- web/index.php
+++ web/index.php
@@ -3,37 +3,56 @@ require 'Slim-2.6.2/Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
 
 $app = new \Slim\Slim();
- 
+
 $app->contentType('application/json; charset=utf-8');
- 
+
 $app->get('/', function() {
             echo "Welcome to mirror-webhook.";
         });
   
 $app->post('/hook',function() use($app){
-            $body = $app->request->getBody();
 
-            $update_script = "/home/fiwareulpgc/mirrors/script/update-mirror.py";
+    $update_script = "/home/fiwareulpgc/mirrors/script/update-mirror.py";
+    $body = $app->request->getBody();
 
-            $output = shell_exec("sudo -u fiwareulpgc ".$update_script." '".$body."'");
+    $command = "sudo -u fiwareulpgc ".$update_script;
+    $command .= " '".$body."' 2>&1";
 
-            echo $body;
+    exec($command, $output, $return_var);
 
-            $app->response->setStatus(200);
+    if ($return_var == 0){
+        echo "OK";
+        $app->response->setStatus(200);
+    }
+    else{
+        error_log(implode("\n", $output));
+        echo "Internal server error";
+        $app->response->setStatus(500);
+    }
+
 });
 
 $app->post('/deny-pull-request',function() use($app){
-            $body = $app->request->getBody();
 
-            $deny_script = "/home/fiwareulpgc/mirrors/script/deny-pull-requests.py";
-            $github_token_file = "/home/fiwareulpgc/mirrors/script/utils/token-fiware.txt";
+    $deny_script = "/home/fiwareulpgc/mirrors/script/deny-pull-requests.py";
+    $github_token_file = "/home/fiwareulpgc/mirrors/script/utils/github-token";
+    $body = $app->request->getBody();
 
-            $output = shell_exec("sudo -u fiwareulpgc ".$deny_script." '".$body."' ".$github_token_file);
+    $command = "sudo -u fiwareulpgc ".$deny_script;
+    $command .= " '".$body."' ".$github_token_file." 2>&1";
 
-            echo $body;
+    exec($command, $output, $return_var);
 
-            $app->response->setStatus(200);
+    if ($return_var == 0){
+        echo "OK";
+        $app->response->setStatus(200);
+    }
+    else{
+        error_log(implode("\n", $output));
+        echo "Internal server error";
+        $app->response->setStatus(500);
+    }
 });
- 
+
 $app->run();
 ?>
