b85850bc12bda6f52a05535bc1c18167e0eec173
Fiware ULPGC <fiware@ulpgc.es>
2016-05-20 15:31:03 +0100

    Update code formatting to be PEP-8 compliant

--- script/deny-pull-requests.py
+++ script/deny-pull-requests.py
@@ -12,61 +12,65 @@ sys.path.append(os.path.dirname(__file__))
 from utils.githubmirrorutils import GithubMirrorUtils
 from utils.utils import *
 
+
 def deny_pull_requests(payload_or_payload_file, token_file):
-	
-	try:
-		body = json.loads(payload_or_payload_file)
-	except ValueError as error:
-		with open(payload_or_payload_file, "r") as f:
-			body = json.load(f)
 
-	directory = os.path.dirname(os.path.abspath(__file__))
-	with open(directory+"/conf.yaml", "r") as f:
-		config = yaml.load(f)
-		config["workspace"] = os.path.expanduser(config["workspace"])
+    try:
+        body = json.loads(payload_or_payload_file)
+    except ValueError as error:
+        with open(payload_or_payload_file, "r") as f:
+            body = json.load(f)
 
-	mirror_remote_url_from_body = body['repository']['ssh_url']
-	mirror_user_repo_name = None
+    directory = os.path.dirname(os.path.abspath(__file__))
+    with open(directory+"/conf.yaml", "r") as f:
+        config = yaml.load(f)
+        config["workspace"] = os.path.expanduser(config["workspace"])
 
-	for repo_clone_url in config['source_repo_urls']:
-		mirror_remote_url = config['source_repo_urls'][repo_clone_url]["mirror_remote_url"]
+    mirror_remote_url_from_body = body['repository']['ssh_url']
+    mirror_user_repo_name = None
 
-		if mirror_remote_url == mirror_remote_url_from_body:
-			src_url = repo_clone_url
-			mirror_user_repo_name = get_user_repo_from_github_url(mirror_remote_url)
-		
-			print mirror_user_repo_name
-			break
+    for repo_clone_url in config['source_repo_urls']:
+        mirror_remote_url =\
+            config['source_repo_urls'][repo_clone_url]["mirror_remote_url"]
 
-	if mirror_user_repo_name is None:
-		print "Entry {} not found in configuration file.".format(mirror_remote_url_from_body)
-		return
+        if mirror_remote_url == mirror_remote_url_from_body:
+            src_url = repo_clone_url
+            mirror_user_repo_name =\
+                get_user_repo_from_github_url(mirror_remote_url)
 
-	g = GithubMirrorUtils(tokenfile=token_file)
+            print mirror_user_repo_name
+            break
 
-	org_name = mirror_user_repo_name['user']
-	repo_name = mirror_user_repo_name['repo']
-	pull_request_number = body['pull_request']['number']
+    if mirror_user_repo_name is None:
+        print ("Entry {} not found in configuration "
+               "file.").format(mirror_remote_url_from_body)
+        return
 
-	print "org_name:", org_name
-	print "repo_name:", repo_name
-	print "pull_request_number:", pull_request_number
-	print "src_url:", src_url
+    g = GithubMirrorUtils(tokenfile=token_file)
 
-	g.close_pull_request(org_name, repo_name, pull_request_number, src_url)
+    org_name = mirror_user_repo_name['user']
+    repo_name = mirror_user_repo_name['repo']
+    pull_request_number = body['pull_request']['number']
+
+    print "org_name:", org_name
+    print "repo_name:", repo_name
+    print "pull_request_number:", pull_request_number
+    print "src_url:", src_url
+
+    g.close_pull_request(org_name, repo_name, pull_request_number, src_url)
 
 
 if __name__ == "__main__":
 
-	if len(sys.argv) < 3: 
-		print "Less than two arguments were passed."
-		sys.exit(-1)
+    if len(sys.argv) < 3:
+        print "Less than two arguments were passed."
+        sys.exit(-1)
 
-	elif len(sys.argv) > 3: 
-		print "Only 2 arguments are accepted."
-		sys.exit(-2)
+    elif len(sys.argv) > 3:
+        print "Only 2 arguments are accepted."
+        sys.exit(-2)
 
-	payload_or_payload_file = sys.argv[1]
-	token_file = sys.argv[2]
+    payload_or_payload_file = sys.argv[1]
+    token_file = sys.argv[2]
 
-	deny_pull_requests(payload_or_payload_file, token_file)
+    deny_pull_requests(payload_or_payload_file, token_file)
--- script/update-def-mirrors.py
+++ script/update-def-mirrors.py
@@ -13,25 +13,30 @@ from utils.githubmirrorutils import GithubMirrorUtils
 
 pr_hook_url = 'http://130.206.84.20/mirror/deny-pull-request'
 
+
 def update_def_mirrors(token_file):
 
-	directory = os.path.dirname(os.path.abspath(__file__))
+    directory = os.path.dirname(os.path.abspath(__file__))
 
-	with open(directory+"/conf.yaml", "r") as f:
-		config = yaml.load(f)
-		config["workspace"] = os.path.expanduser(config["workspace"])
+    with open(directory+"/conf.yaml", "r") as f:
+        config = yaml.load(f)
+        config["workspace"] = os.path.expanduser(config["workspace"])
 
-	for repo_clone_url in config['source_repo_urls']:
-		mirror_remote_url = config['source_repo_urls'][repo_clone_url]["mirror_remote_url"]
-		mirror_user_repo_name = get_user_repo_from_github_url(mirror_remote_url)
-		
-		print mirror_user_repo_name
+    for repo_clone_url in config['source_repo_urls']:
+        mirror_remote_url =\
+            config['source_repo_urls'][repo_clone_url]["mirror_remote_url"]
+        mirror_user_repo_name =\
+            get_user_repo_from_github_url(mirror_remote_url)
 
-		g = GithubMirrorUtils(tokenfile=token_file)
-		g.create_update_mirror_repo(mirror_user_repo_name['user'], mirror_user_repo_name['repo'], repo_clone_url, pr_hook_url)
+        print mirror_user_repo_name
+
+        g = GithubMirrorUtils(tokenfile=token_file)
+        g.create_update_mirror_repo(mirror_user_repo_name['user'],
+                                    mirror_user_repo_name['repo'],
+                                    repo_clone_url,
+                                    pr_hook_url)
 
 
 if __name__ == "__main__":
 
-	update_def_mirrors(sys.argv[1])
-
+    update_def_mirrors(sys.argv[1])
